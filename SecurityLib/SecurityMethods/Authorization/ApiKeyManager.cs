02c8f208aae2f00035bb5dedf43266fbf6cbbba8
Patrick.B <patrick.balian@mailbox.tu-dresden.de>
2024-11-21 12:43:10 +0100

    structure the security module

--- SecurityLib/SecurityMethods/Authorization/ApiKeyManager.cs
+++ SecurityLib/SecurityMethods/Authorization/ApiKeyManager.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace SecurityLib.SecurityMethods.Authorization
 {
-    internal class ApiKeyManager
+    public class ApiKeyManager
     {
+        private readonly ILogger<ApiKeyManager> _logger;
+        private readonly IConfiguration _configuration;
+        public ApiKeyManager(ILogger<ApiKeyManager> logger, IConfiguration configuration) 
+        {
+            _configuration = configuration;
+            _logger = logger;
+        }
+
+        public void requestToken()
+        {
+
+        }
+
+        public void revokeToken()
+        {
+
+        }
+
+        public void inspect()
+        {
+
+        }
+
     }
 }
